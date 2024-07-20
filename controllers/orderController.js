f66540bc4101c524db6af7cac3c998202ecfd006
aliji865030 <abbasalichand786@gmail.com>
2024-07-20 09:46:17 +0530

    modification

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -13,7 +13,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 const placeOrder = async (req, res) => {
     console.log("Request received:", req.body);
     
-    const frontend_url = "https://taste-trekker.vercel.app";
+    const frontend_url = "https://localhost:4000";
 
     try {
         const newOrder = new orderModel({
@@ -51,10 +51,10 @@ const placeOrder = async (req, res) => {
         const session = await stripe.checkout.sessions.create({
             line_items: line_items,
             mode: "payment",  // Corrected parameter
-            // success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-            // cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`
-            success_url: `${frontend_url}/myorders`,
-            cancel_url: `${frontend_url}/myorders`
+            success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`
+            // success_url: `${frontend_url}/myorders`,
+            // cancel_url: `${frontend_url}/myorders`
         });
 
         res.json({
